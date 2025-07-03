224575cba517d8d223c208f496eb77d54d34b535
OsmarReis <osmarzero26@gmail.com>
2025-07-03 17:21:04 -0300

    Added more CSS rules and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Repertoires } from './pages/repertoires/repertoires';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: Home},
+  {path: "repertoires", component: Repertoires},
+];
